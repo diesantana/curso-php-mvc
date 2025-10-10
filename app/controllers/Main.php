0451c1f56fced52402120711e665adfb9ee16c05
Diego Santana <3c.santana@gmail.com>
2025-10-09 22:02:21 -0300

    feat: Implementando o autoload com PSR-4 e composer

---
+++ app/controllers/Main.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BNG\Controllers;
+
+class Main{
+    public function teste() {
+        echo 'Main.php diz: Apredendo a configurar o autoload';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello index.php';
+// require_once('../app/config.php');
+// require_once('../app/controllers/Main.php');
+require_once('../vendor/autoload.php');
+use BNG\Controllers\Main as MainController;
+
+echo APP_NAME;
+echo '<br>';
+$controller = new MainController();
+echo $controller->teste();

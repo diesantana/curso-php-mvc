9a97bc6ee38e29c32b728af0a48f9fc99ba77bef
Diego Santana <3c.santana@gmail.com>
2025-10-21 21:23:11 -0300

    feat: Add favicon

--- app/views/layouts/html_header.php
+++ app/views/layouts/html_header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/logo_32.png" type="image/png">
     <title><?= APP_NAME ?></title>
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">

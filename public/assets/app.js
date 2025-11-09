e6d5c2b8ab61d0412354c9a57b7b8a7051caea1a
Diego Santana <3c.santana@gmail.com>
2025-11-09 07:33:47 -0300

    feat: Implementa funções de testes para agilizar o login

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,1 +1,9 @@
-// my custom javascript
+function admin() {
+    document.getElementById('text_username').value = 'admin@bng.com';
+    document.getElementById('text_password').value = 'Aa123456';
+}
+
+function agente1() {
+    document.getElementById('text_username').value = 'agente1@bng.com';
+    document.getElementById('text_password').value = 'Aa123456';
+}

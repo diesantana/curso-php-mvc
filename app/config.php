f42018c11237e6c8ad0aff12b5097fa6761c8db5
Diego Santana <3c.santana@gmail.com>
2025-11-08 21:42:01 -0300

    feat: Armazena o caminho de destino do arquivo de logs

--- app/config.php
+++ app/config.php
@@ -10,3 +10,6 @@ define('MYSQL_PASSWORD', ''); // criamos este usuário sem senha
 
 // Chave para descriptografar os dados
 define('MYSQL_AES_KEY', 'Vduu47qL51hLn6bkYkY6NlO1nivsmdfD');
+
+// Destino do arquivo de log
+define('LOGS_PATH', __DIR__ . '/../logs/app.log');

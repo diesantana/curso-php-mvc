0d2e688264d1d522fb6ed0a56c19bf1f95c2c20f
Diego Santana <3c.santana@gmail.com>
2025-11-13 20:11:48 -0300

    feat: Implementa o Agent Controler com o método my_clients()

---
+++ app/controllers/Agent.php
@@ -0,0 +1,38 @@
+<?php 
+namespace bng\Controllers;
+
+use bng\Models\Agents;
+
+/**
+ * Agent Controller - Responsável pelas operações relacionadas a entidade Agent 
+ */
+class Agent extends BaseController {
+
+    /**
+     * Exibe os dados dos clientes de acordo com o agent
+     */
+    public function my_clients() {
+        // Verificação de segurança, garantido que apenas agentes autenticados acessem o método
+        if(!checkSession() || $_SESSION['user']->profile != 'agent') {
+            header('Location: index.php'); // redirecionada para o index.php que consequentemente vai chamar o método index() do controlador main
+        }
+        
+        // ID do usuário logado
+        $agentId = $_SESSION['user']->id;
+        // Cria uma instância do model
+        $agentModel = new Agents();
+        // Busca os clientes através do método get_agent_clients no model
+        $results = $agentModel->get_agent_clients($agentId);
+
+        // Armazena os dados para serem utilziados na view
+        $data['user'] = $_SESSION['user'];
+        $data['clients'] = $results['data'];
+
+        // Renderiza a view "agent_clients", responsável pela exibição dos clientes
+        $this->view('layouts/html_header'); // Estrutura inicial do HTML
+        $this->view('navbar', $data); // navbar
+        $this->view('agent_clients', $data); // exibição dos clientes
+        $this->view('footer'); // footer
+        $this->view('layouts/html_footer'); // Estrutura final do HTML
+    }
+}

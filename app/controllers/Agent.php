4877eba33c12c5be7363092bb7456d966edc1f5f
Diego Santana <3c.santana@gmail.com>
2025-11-15 08:59:24 -0300

    feat: Implementa o método "new_client_form()", responsável por renderizar o formulário de cadastro de clientes

--- app/controllers/Agent.php
+++ app/controllers/Agent.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace bng\Controllers;
 
 use bng\Models\Agents;
@@ -6,17 +7,19 @@ use bng\Models\Agents;
 /**
  * Agent Controller - Responsável pelas operações relacionadas a entidade Agent 
  */
-class Agent extends BaseController {
+class Agent extends BaseController
+{
 
     /**
      * Exibe os dados dos clientes de acordo com o agent
      */
-    public function my_clients() {
+    public function my_clients()
+    {
         // Verificação de segurança, garantido que apenas agentes autenticados acessem o método
-        if(!checkSession() || $_SESSION['user']->profile != 'agent') {
+        if (!checkSession() || $_SESSION['user']->profile != 'agent') {
             header('Location: index.php'); // redirecionada para o index.php que consequentemente vai chamar o método index() do controlador main
         }
-        
+
         // ID do usuário logado
         $agentId = $_SESSION['user']->id;
         // Cria uma instância do model
@@ -32,6 +35,28 @@ class Agent extends BaseController {
         $this->view('layouts/html_header'); // Estrutura inicial do HTML
         $this->view('navbar', $data); // navbar
         $this->view('agent_clients', $data); // exibição dos clientes
+        $this->view('footer'); // footer
+        $this->view('layouts/html_footer'); // Estrutura final do HTML
+    }
+
+    /**
+     * Renderiza o formulário de inserção de novos clientes
+     */
+    public function new_client_frm()
+    {
+        // Verifica se a sessão está ativa e se o usuário logado é um agente
+        if (!checkSession() || $_SESSION['user']->profile != 'agent') {
+            // redireciona para a página inicial (login)
+            header('Location: index.php');
+        }
+
+        // armazena os dados do usuário logado
+        $data['user'] = $_SESSION['user'];
+
+        // Renderiza as views
+        $this->view('layouts/html_header'); // Estrutura inicial do HTML
+        $this->view('navbar', $data); // navbar
+        $this->view('insert_client_frm'); // formulário de cadastro
         $this->view('footer'); // footer
         $this->view('layouts/html_footer'); // Estrutura final do HTML
     }

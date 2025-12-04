847474540453b09860e443f3bb0f168229024394
Diego Santana <3c.santana@gmail.com>
2025-12-04 20:15:33 -0300

    feat: Implementa a exibição dos dados do cliente a ser editado

--- app/views/edit_client_frm.php
+++ app/views/edit_client_frm.php
@@ -10,54 +10,62 @@
 
                         <hr>
 
-                        <form action="#" method="post">
+                        <form action="?ct=agent&mt=edit_client_submit" method="post">
 
-                        <input type="hidden" name="id_client" value="">
-
-                            <div class="mb-3">
-                                <label for="text_name" class="form-label">Nome</label>
-                                <input type="text" name="text_name" id="text_name" value="" class="form-control" required>
+                        <!-- ID  -->
+                        <input type="hidden" name="id_client" value="<?=htmlspecialchars($client->id)?>">
+                        
+                        <!-- NOME  -->
+                        <div class="mb-3">
+                            <label for="text_name" class="form-label">Nome</label>
+                                <input type="text" name="text_name" id="text_name" value="<?=htmlspecialchars($client->name)?>" class="form-control" required>
                             </div>
 
                             <div class="row mb-3">
+                                <!-- GENERO -->
                                 <div class="col-md-6 col-sm-12">
                                     <div>Sexo</div>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="radio_gender" id="radio_m" value="m" checked>
+                                        <input class="form-check-input" type="radio" name="radio_gender" id="radio_m" value="m"  <?=  $client->gender == 'm' ? 'checked' : '' ?>>
                                         <label class="form-check-label" for="radio_m">Masculino</label>
                                     </div>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="radio_gender" id="radio_f" value="f">
+                                        <input class="form-check-input" type="radio" name="radio_gender" id="radio_f" value="f"  <?=  $client->gender == 'f' ? 'checked' : '' ?>>
                                         <label class="form-check-label" for="radio_f">Feminino</label>
                                     </div>
                                 </div>
+                                <!-- DATA DE NASCIMENTO -->
                                 <div class="col-md-6 col-sm-12">
                                     <label for="text_birthdate" class="form-label">Data de nascimento</label>
-                                    <input type="text" class="form-control" name="text_birthdate" id="text_birthdate" value="" required>
+                                    <input type="text" class="form-control" name="text_birthdate" id="text_birthdate" value="<?=htmlspecialchars($client->birthdate)?>" required>
                                 </div>
                             </div>
 
                             <div class="row mb-3">
+                                <!-- EMAIL -->
                                 <div class="col-md-6 col-sm-12">
                                     <label for="text_email" class="form-label">Email</label>
-                                    <input type="email" class="form-control" name="text_email" id="text_email" values="" required>
+                                    <input type="email" class="form-control" name="text_email" id="text_email" value="<?=htmlspecialchars($client->email)?>" required>
                                 </div>
+                                <!-- TELEFONE -->
                                 <div class="col-md-6 col-sm-12">
                                     <label for="text_phone" class="form-label">Telefone</label>
-                                    <input type="text" class="form-control" name="text_phone" id="text_phone" values="" required>
+                                    <input type="text" class="form-control" name="text_phone" id="text_phone" value="<?=htmlspecialchars($client->phone)?>" required>
                                 </div>
                             </div>
-
+                            <!-- INTERESSES -->
                             <div class="mb-3">
                                 <label for="text_interests" class="form-label">Interesses<span class="ms-4"><small>(Palavras separadas por virgulas)</small></span></label>
-                                <input type="text" class="form-control" name="text_interests" id="text_interests" value="">
+                                <input type="text" class="form-control" name="text_interests" id="text_interests" value="<?=htmlspecialchars($client->interests)?>">
                             </div>
                             
+                            <!-- CANCELAR E SUBMIT -->
                             <div class="mb-3 text-center">
                                 <a href="#" class="btn btn-secondary"><i class="fa-solid fa-xmark me-2"></i>Cancelar</a>
                                 <button type="submit" class="btn btn-secondary"><i class="fa-regular fa-floppy-disk me-2"></i>Atualizar</button>
                             </div>
 
+                            <!-- MENSAGENS DE ERRO -->
                             <div class="alert alert-danger p-2 text-center">
                                 [mensagem de erro]
                             </div>
@@ -70,3 +78,8 @@
         </div>
     </div>
 </div>
+<script>
+    flatpickr("#text_birthdate", {
+        dateFormat: "d-m-Y"
+    })
+</script>

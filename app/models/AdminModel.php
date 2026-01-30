33ef01843955c88a2b5c3f889254e71e2849fe09
Diego Santana <3c.santana@gmail.com>
2026-01-30 04:20:00 -0300

    feat: Cria o AdminModel

---
+++ app/models/AdminModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace bng\Models;
+
+class AdminModel extends BaseModel {
+    public function get_all_clients() {
+        $this->db_connect(); // Conexão com a base de dados
+        // MONTAR O SQL AQUI...
+    }
+}

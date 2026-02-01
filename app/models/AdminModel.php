0c3487c2278f65ffc777cb5b2e6d6603c7d2746b
Diego Santana <3c.santana@gmail.com>
2026-02-01 20:55:52 -0300

    feat: Implementa a lógica para buscar os clientes com os seus respectivos agentes na base de dados

--- app/models/AdminModel.php
+++ app/models/AdminModel.php
@@ -2,9 +2,32 @@
 
 namespace bng\Models;
 
-class AdminModel extends BaseModel {
-    public function get_all_clients() {
+class AdminModel extends BaseModel
+{
+    /**
+     * Busca Todos os clientes na base de dados, com os seus respectivos agentes. 
+     * @return Array Array contendo os dados dos clientes em formato stdClass Object
+     */
+    public function get_all_clients() :array
+    {
         $this->db_connect(); // Conexão com a base de dados
-        // MONTAR O SQL AQUI...
+        $sql =
+            "SELECT " .
+            "persons.id, " .
+            "AES_DECRYPT(persons.name, '" . MYSQL_AES_KEY . "') as name, " . // criptografia
+            "persons.gender, " .
+            "persons.birthdate, " .
+            "AES_DECRYPT(persons.email, '" . MYSQL_AES_KEY . "') as email, " . // criptografia
+            "persons.interests, " .
+            "persons.created_at, " .
+            "AES_DECRYPT(agents.name, '" . MYSQL_AES_KEY . "') as agent " . // criptografia
+            "FROM persons INNER JOIN agents ON persons.id_agent = agents.id " .
+            "WHERE persons.deleted_at IS NULL " .
+            "ORDER BY persons.created_at DESC";
+
+        // Executa a query
+        $results = $this->query($sql);
+
+        return $results->results;
     }
 }

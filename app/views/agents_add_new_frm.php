5638bcbf5e8c730c5851dba982824e4fec21c430
Diego Santana <3c.santana@gmail.com>
2026-02-09 16:33:28 -0300

    feat: Implementa a lógica para exibição de erros no formulário de inserção de agentes

--- app/views/agents_add_new_frm.php
+++ app/views/agents_add_new_frm.php
@@ -9,6 +9,21 @@
                         <h4><strong>Adicionar novo agente</strong></h4>
 
                         <hr>
+                                                    <!-- Exibe erros de validação -->
+                            <?php if (!empty($validationErrors)): ?>
+                                <div class="alert alert-danger p-2 text-center">
+                                    <ul>
+                                        <?php foreach ($validationErrors as $error): ?>
+                                            <li><?= $error ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </div>
+                            <?php endif; ?>
+
+                            <!-- Exibe erros no servidor -->
+                            <?php if (!empty($serverErrors)): ?>
+                                <div class="alert alert-danger p-2 text-center"><?= $serverErrors ?></div>
+                            <?php endif; ?>
 
                         <form action="#" method="post">
 
@@ -21,17 +36,13 @@
                                 <label for="select_profile" class="form-label">Perfil</label>
                                 <select name="select_profile" id="select_profile" class="form-control" required>
                                     <option value="admin">Administrador</option>
-                                    <option value="agente">Agente</option>
+                                    <option value="agent">Agente</option>
                                 </select>
                             </div>
-                            
+
                             <div class="mb-3 text-center">
                                 <a href="#" class="btn btn-secondary"><i class="fa-solid fa-xmark me-2"></i>Cancelar</a>
                                 <button type="submit" class="btn btn-secondary"><i class="fa-solid fa-user-plus me-2"></i>Criar agente</button>
-                            </div>
-
-                            <div class="alert alert-danger p-2 text-center">
-                                [mensagem de erro]
                             </div>
 
                         </form>

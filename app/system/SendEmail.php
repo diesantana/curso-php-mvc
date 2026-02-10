3265cade36f4a4267efe6060e6f178e287f758fc
Diego Santana <3c.santana@gmail.com>
2026-02-10 12:36:41 -0300

    feat: Implementa o método sendEmail() responsável por encapsular a lógica do PHPMailer

--- app/system/SendEmail.php
+++ app/system/SendEmail.php
@@ -4,7 +4,56 @@ declare(strict_types=1);
 
 namespace bng\System;
 
-class SendEmail {
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class SendEmail
+{
+    /**
+     * Envia um e-mail (HTML) para um ou mais destinarátios usando PHPMailer.
+     * @param array $recipients Lista de destinatários.
+     * @param string $subject Assunto do email.
+     * @param string $emailBody Corpo do email (HTML).
+     * @param string $altEmailBody Corpo alternativo para o email (txt).
+     * @return array Retorna um array contendo status da operação (success ou error) e mensagem.
+     */
+    public function sendEmail(array $recipients, string $subject, string $emailBody, string $altEmailBody): array
+    {
+        $mail = new PHPMailer(true); // Instancia o obj e ativa as exceções
+
+        try {
+            //Configurações do servidor
+            /*
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                          //Enable verbose debug output
+            $mail->isSMTP();                                                //Send using SMTP
+            $mail->Host       = EMAIL_HOST;                                 //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                       //Enable SMTP authentication
+            $mail->Username   = EMAIL_USERNAME;                             //SMTP username
+            $mail->Password   = EMAIL_PASSWORD;                             //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                //Enable implicit TLS encryption
+            $mail->Port       = EMAIL_PORT;                                 //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            */
+
+            $mail->CharSet = 'UTF-8';
+            $mail->setFrom(EMAIL_FROM, EMAIL_FROM_NAME); // Autor do email
+
+            //Destinatários
+            foreach ($recipients as $email) {
+                $mail->addAddress($email);
+            }
+
+            // Conteúdo
+            $mail->isHTML(true);  // Formato HTML
+            $mail->Subject = $subject; // Assunto do email
+            $mail->Body    = $emailBody; // Corpo do email (html)
+            $mail->AltBody = $altEmailBody; // Corpo do email alternativo
+
+            $mail->send(); // Envia o email
+            return ['status' => 'success', 'message' => 'Email enviado com sucesso'];
+        } catch (Exception $e) {
+            return ['status' => 'error', 'message' => $mail->ErrorInfo];
+        }
+    }
 
 
     /**
@@ -12,7 +61,8 @@ class SendEmail {
      * @param string $link URL para o cadastro de senha.
      * @return string Corpo do email em formato HTML. 
      */
-    private function generate_email_body(string $link): string {
+    private function generate_email_body(string $link): string
+    {
         $html = '<p>Para concluir o processo de registro de agente, clique no link abaixo:</p>';
         $html .= "<a href='{$link}'>Concluir registro de agente</a>";
         return $html;

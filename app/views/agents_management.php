c0fe9b9d1c57a20cdddd1a5a33aaf7c8e6093059
Diego Santana <3c.santana@gmail.com>
2026-02-20 20:36:52 -0300

    feat: Configura a rota para exportação de dados dos agentes na view

--- app/views/agents_management.php
+++ app/views/agents_management.php
@@ -122,7 +122,7 @@
                         <p class="mb-5">Total: <strong><?= count($agentsData) ?></strong></p>
                     </div>
                     <div class="col text-end">
-                        <a href="#" class="btn btn-secondary px-4"><i class="fa-regular fa-file-excel me-2"></i>Exportar para XLSX</a>
+                        <a href="?ct=admincontroller&mt=export_global_agents_xlsx" class="btn btn-secondary px-4"><i class="fa-regular fa-file-excel me-2"></i>Exportar para XLSX</a>
                         <a href="?ct=main&mt=index" class="btn btn-secondary px-4"><i class="fa-solid fa-chevron-left me-2"></i>Voltar</a>
                     </div>
                 </div>
